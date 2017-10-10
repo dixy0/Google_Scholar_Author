191bf3b34bd5b86cd931e0119e3037b2ca441ece
Xin Di <synge.x.d@gmail.com>
2017-10-09 23:08:30 -0400

    Add function of finding coauthors

---
+++ get_coauthors.m
@@ -0,0 +1,23 @@
+function CAIDs = get_coauthors(GSID)
+% Given a Google Scholar ID, return Google Scholar IDs of listed co-authors
+% For example: oa = get_coauthors('wDjD46gAAAAJ')
+
+if nargin == 0
+    GSID = 'wDjD46gAAAAJ';
+end
+
+str = urlread(['http://scholar.google.com/citations?user=' GSID]);
+pat = '?user=.......AAAAJ';
+
+S = regexp(str,pat);
+if length(S) > 1
+    S = S(2:end);
+    
+    for i = 1:length(S)
+        CAIDs{i,1} = str(S(i)+6:S(i)+17);
+    end
+else
+    CAIDs = [];
+end
+
+
